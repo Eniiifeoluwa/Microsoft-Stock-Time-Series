e8330e7b4cd660f2c49b5af0e7cf7a33b5ac7c4f
unknown <akinolaayomide226@gmail.com>
2025-07-31 23:07:48 +0100

    Tracker added.

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, ElasticNet, SGDRegressor
 import joblib
 import json
 
@@ -29,6 +29,8 @@ def train_and_evaluate(config_path):
     testing_set.set_index('Date', inplace= True)
     X_train, Y_train = training_set.drop(columns= [target]), training_set[target]
     X_test, Y_test = testing_set.drop(columns= [target]), testing_set[target]
+    alpha = config['estimators']['params']['alpha']
+    l1_ratio = config['estimators']['params']['l1_ratio']
     scaler = MinMaxScaler()
     X_scaled = scaler.fit_transform(X_train.values)
     model = LinearRegression()
@@ -39,6 +41,7 @@ def train_and_evaluate(config_path):
     Y_pred = model.predict(X_test_scaled)
     rmse, mse, r2 = evaluate(Y_pred, Y_test)
     score_file = config['report']['scores']
+    params_file = config['report']['params']
     with open(score_file, 'w') as f:
         scores= {
             'RMSE': rmse,
@@ -46,6 +49,12 @@ def train_and_evaluate(config_path):
             'R2': r2
             }
         json.dump(scores, f, indent= 4)
+    with open(params_file, 'w') as f:
+        params = {
+            'alpha': alpha,
+            'l1_ratio': l1_ratio
+        }
+        json.dump(params, f, indent= 4)
 
 if __name__ == "__main__":
     import argparse
