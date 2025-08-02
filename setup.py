36d96fb2323d4e21e03116774d440a6b49be3b79
unknown <akinolaayomide226@gmail.com>
2025-08-02 14:31:57 +0100

    Setup file updated.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup( name= "src",
+      author= 'Akinola Afolabi',
+      version= "0.0.0.0",
+      author_email= "akinolaayomide226@gmail.com",
+      description= "This is a Microsoft Time Series project.",
+      packages= find_packages(),
+      license= 'MIT',
+      
+
+)
