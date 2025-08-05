85a97231f6f5bd20f430a804301f6e4e0607bc32
unknown <akinolaayomide226@gmail.com>
2025-08-05 22:13:01 +0100

    Testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,12 @@ import pytest
 import os
 
 @pytest.fixture
-
 def config(config_path = 'params.yaml'):
     with open(config_path) as l:
         config = yaml.safe_load(l)
     return config
 
 @pytest.fixture
-
 def get_schema(schema_path = 'schema_in.json'):
     with open(schema_path) as s:
         schema = json.load(s)
